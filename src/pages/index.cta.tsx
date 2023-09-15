0acf2340519730cbaa98677cf2d2a257395c70f9
Alpha <piotr@piotryordanov.com>
2023-09-15 15:26:06 +0400

    feat: prepare to deploy

---
+++ src/pages/index.cta.tsx
@@ -0,0 +1,24 @@
+const CTA = () => {
+  return (
+    <div className=''>
+      <div className='text-white'>
+        <div className='mb-8 text-5xl text-cyan-300'>AI Trading BOT</div>
+        <div className='mb-8'>Grow your wealth, One Trade at a time!</div>
+        <div className='mb-8'>
+          Rocket bot is an AI powered trading bot specialized in speculative
+          trading. It aims to generate high yields for the investor who has high
+          risk appetite
+        </div>
+      </div>
+
+      <a
+        href='https://collective2.com/details/145806926'
+        className='mr-2 rounded-lg bg-cyan-500 px-4 py-2 text-sm font-medium text-black hover:bg-cyan-400 focus:outline-none focus:ring-4 lg:px-5 lg:py-2.5'
+      >
+        Start Trading
+      </a>
+    </div>
+  );
+};
+
+export default CTA;
